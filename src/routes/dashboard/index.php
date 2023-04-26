b56ca5208d8ba6eaf6ffe296c4a991becd07f536
A_Rijenth <a_rijenth@laposte.net>
2023-04-26 20:47:40 +0200

    remove javascript listener (#19)

--- src/routes/dashboard/index.php
+++ src/routes/dashboard/index.php
@@ -18,17 +18,23 @@ if($username === null || $email === null) {
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = $_SESSION['name'];
+
   $file = $_FILES['fileToUpload'];
 
-  $shellCommande = new shellCommande();
+  if (!isset($_FILES['fileToUpload']) || $_FILES['fileToUpload']['error'] === UPLOAD_ERR_NO_FILE) {
+    echo '<script>alert("Please select a file to upload!")</script>';
+  } else {
 
-  try {
-    $shellCommande->uploadFile($username, $file);
-    
-    echo '<script>alert("Upload successful!")</script>';
-    
-  } catch (Exception $e) {
-    echo '<script>alert(' . "Error: " . $e->getMessage() .')</script>';
+    $shellCommande = new shellCommande();
+
+    try {
+      $shellCommande->uploadFile($username, $file);
+
+      echo '<script>alert("Upload successful!")</script>';
+
+    } catch (Exception $e) {
+      echo '<script>alert(' . "Error: " . $e->getMessage() .')</script>';
+    }
   }
 }
 
@@ -42,14 +48,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard</title>
     <link rel="stylesheet" href="../../assets/styles/dashboard.css" />
-    <script>
-      const uploadFileButton = document.getElementById('uploadFile');
-      const fileInput = document.getElementById('fileToUpload');
-
-      uploadFileButton.addEventListener('click', () => {
-        fileInput.click();
-      });
-  </script>
   </head>
   <body>
     <main id="dashboard">
@@ -57,19 +55,20 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         <h1 class="user-welcome">Bonjour <?= $username ?></h1>
         <p class="user-email"><?= $email ?></p>
         <a href="../logout.php">Deconnexion</a>  
-      </aside>
-      <div id="files-n-folder">
-        <form action="#" method="POST" enctype="multipart/form-data">
-          <input type="file" name="fileToUpload" id="fileToUpload" style="display: none;">
+
+        <form method="POST" enctype="multipart/form-data">
+          <input type="file" name="fileToUpload" id="fileToUpload">
           <button id="createFolder" type="submit" name="submit">
             <img src="../../assets/img/createFolder.svg" alt="folder" />
           </button>
         </form>
 
-        <button id="uploadFile">
+        <!-- <button id="uploadFile">
           <img src="../../assets/img/upload.svg" alt="file" />
-        </button>
-
+        </button> -->
+        
+      </aside>
+      <div id="files-n-folder">
         <div class="folder">
           <img src="../../assets/img/folder.svg" alt="folder" />
           <p>Folder 1</p>
