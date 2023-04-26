60d3f5caa5a5646cca7fbef98e17f754cd85e989
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-04-26 09:32:57 +0200

    🔨 : shell and batch added

---
+++ azure.gitpull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SSH_SERVER=74.234.18.118
+SSH_USER=groupe3
+SSH_PASSWORD=HeticGroupe3!
+
+echo "Connecting to SSH server $SSH_SERVER..."
+sshpass -p "$SSH_PASSWORD" ssh "$SSH_USER"@"$SSH_SERVER" "cd /var/www/html/serveur-nginx && git pull"
+echo "Git pull command completed."
