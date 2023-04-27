380bf1b49a260c90a7d8f9363df76b5e331b6363
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-04-27 09:34:47 +0200

    🔥 : removed unused .bat & .sh files

--- azure.gitpull.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set SSH_SERVER=74.234.18.118
-set SSH_USER=groupe3
-set SSH_PASSWORD=HeticGroupe3!
-
-echo Connecting to SSH server %SSH_SERVER%...
-ssh -p %SSH_PASSWORD% ssh %SSH_USER%@%SSH_SERVER% "cd /var/www/html/serveur-nginx && git pull"
-echo Git pull command completed.
