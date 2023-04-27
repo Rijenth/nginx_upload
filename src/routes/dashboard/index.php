c9f22199fe6109d471f8dc39aa6d9a2e958ed5f1
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-04-27 11:33:03 +0200

    correction upload

--- src/routes/dashboard/index.php
+++ src/routes/dashboard/index.php
@@ -33,47 +33,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Dashboard</title>
-  <link rel="stylesheet" href="../../assets/styles/dashboard.css"/>
+  <link rel="stylesheet" href="../../assets/styles/dashboard.css" />
 </head>
+
 <body>
-<main id="dashboard">
-  <aside id="user-data">
-    <form id="uploadForm" method="POST" enctype="multipart/form-data">
-      <h1 class="user-welcome">Bonjour <?= $username ?></h1>
-      <p class="user-email"><?= $email ?></p>
-      <p class="user-account-size">Taille de votre repertoire: <?= $dashboard_data['account_size'] ?></p>
-      <p class="user-database-size">Taille de la base de données: <?= $dashboard_data['database_size'] ?></p>
-      <a href="../logout.php">Deconnexion</a>
+  <main id="dashboard">
+    <aside id="user-data">
+      <form id="uploadForm" method="POST" enctype="multipart/form-data">
+        <h1 class="user-welcome">Bonjour <?= $username ?></h1>
+        <p class="user-email"><?= $email ?></p>
+        <p class="user-account-size">Taille de votre repertoire: <?= $dashboard_data['account_size'] ?></p>
+        <p class="user-database-size">Taille de la base de données: <?= $dashboard_data['database_size'] ?></p>
+        <a href="../logout.php">Deconnexion</a>
 
-      <label for="fileToUpload">
-        <button id="uploadFile" type="button">
-          <img src="../../assets/img/upload.svg" alt="folder"/>
-        </button>
-      </label>
-      <input type="file" name="fileToUpload" id="fileToUpload" style="display: none;">
-    </form>
-  </aside>
+        <label for="fileToUpload">
+          <button id="uploadFile" type="button">
+            <img src="../../assets/img/upload.svg" alt="folder" />
+          </button>
+        </label>
+        <input type="file" name="fileToUpload" id="fileToUpload" style="display: none;">
+      </form>
+    </aside>
 
-  <div id="files-n-folder">
-    <?php if (empty($user_files)) { ?>
-      <p class="empty-folder">Votre dossier est vide</p>
-    <?php } ?>
+    <div id="files-n-folder">
+      <?php if (empty($user_files)) { ?>
+        <p class="empty-folder">Votre dossier est vide</p>
+      <?php } ?>
 
-    <?php foreach ($user_files as $filename) { ?>
-      <?php $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION)); ?>
-      <?php if (in_array($ext, ['jpg', 'jpeg', 'png', 'gif','pdf','docx','doc','txt','mp3','mp4','mkv','avi','zip','rar','7z','tar','gz','iso','exe','msi','apk','deb','rpm','jar','java','php','html','css','js','py','c','cpp','h','hpp','sh','bat','vbs','sql','xml','json','yml','yaml','ini','cfg','conf','log','bak','bak','bak'])) { ?>
-        <div class="file">
-          <img src="../../assets/img/file.svg" alt="file"/>
-          <p><?= $filename ?></p>
-        </div>
-      <?php } else { ?>
-        <div class="folder">
-          <img src="../../assets/img/folder.svg" alt="folder" />
+      <?php foreach ($user_files as $filename) { ?>
+        <?php $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION)); ?>
+        <?php if (in_array($ext, ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'docx', 'doc', 'txt', 'mp3', 'mp4', 'mkv', 'avi', 'zip', 'rar', '7z', 'tar', 'gz', 'iso', 'exe', 'msi', 'apk', 'deb', 'rpm', 'jar', 'java', 'php', 'html', 'css', 'js', 'py', 'c', 'cpp', 'h', 'hpp', 'sh', 'bat', 'vbs', 'sql', 'xml', 'json', 'yml', 'yaml', 'ini', 'cfg', 'conf', 'log', 'bak', 'bak', 'bak'])) { ?>
+          <div class="file">
+            <img src="../../assets/img/file.svg" alt="file" />
+            <p><?= $filename ?></p>
+          </div>
+        <?php } else { ?>
+          <div class="folder">
+            <img src="../../assets/img/folder.svg" alt="folder" />
             <p><?= $filename ?></p>
           </div>
         <?php } ?>
@@ -81,29 +83,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
   </main>
   <script>
-  // Get the form and upload button elements
-  const form = document.getElementById('uploadForm');
-  const uploadBtn = document.getElementById('uploadFile');
+    // Get the form and upload button elements
+    const form = document.getElementById('uploadForm');
+    const uploadBtn = document.getElementById('uploadFile');
 
-  // Add event listener for button click
-  uploadBtn.addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent default button behavior
+    // Add event listener for button click
+    uploadBtn.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent default button behavior
 
-    // Create input element for file upload
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.name = 'fileToUpload';
+      // Create input element for file upload
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.name = 'fileToUpload';
 
-    // Add change event listener for file input
-    fileInput.addEventListener('change', function() {
-      // Submit form when file is selected
-      form.submit();
+      // Add change event listener for file input
+      fileInput.addEventListener('change', function() {
+        const file = fileInput.files[0];
+        if (file) {
+          // Submit form when file is selected
+          form.submit();
+        } else {
+          alert('Please select a file to upload!');
+        }
+      });
+
+      // Click the file input to open the file upload dialog
+      fileInput.click();
     });
-
-    // Click the file input to open the file upload dialog
-    fileInput.click();
-  });
-</script>
+  </script>
 
 </body>
 
