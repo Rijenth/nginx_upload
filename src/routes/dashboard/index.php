872c64f9cbce09aa24823079d5b664ae01f70ee9
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-04-27 10:02:10 +0200

    ♻️ : refacto dashboard and user flow

--- src/routes/dashboard/index.php
+++ src/routes/dashboard/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require(realpath(__DIR__ . DIRECTORY_SEPARATOR . '../../functions/shellCommande.php'));
 session_start();
 
@@ -8,14 +8,14 @@ $email  = null;
 
 $user_files = [];
 
-if(isset($_SESSION["name"])) {
-    $username = $_SESSION["name"];
+if (isset($_SESSION["name"])) {
+  $username = $_SESSION["name"];
 }
 
-if(isset($_SESSION["email"])) {
+if (isset($_SESSION["email"])) {
   $email = $_SESSION["email"];
 }
-if($username === null || $email === null) {
+if ($username === null || $email === null) {
   header("Location: ../logout.php");
   exit();
 }
@@ -24,7 +24,7 @@ if (isset($_SESSION['user_files'])) {
   $user_files = $_SESSION['user_files'];
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = $_SESSION['name'];
 
   $file = $_FILES['fileToUpload'];
@@ -45,9 +45,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
       $user_files = $_SESSION['user_files'];
 
       echo '<script>alert("Upload successful!")</script>';
-
     } catch (Exception $e) {
-      echo '<script>alert(' . "Error: " . $e->getMessage() .')</script>';
+      echo '<script>alert(' . "Error: " . $e->getMessage() . ')</script>';
     }
   }
 }
@@ -55,52 +54,53 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="../../assets/styles/dashboard.css" />
-  </head>
-  <body>
-    <main id="dashboard">
-      <aside id="user-data">
-        <h1 class="user-welcome">Bonjour <?= $username ?></h1>
-        <p class="user-email"><?= $email ?></p>
-        <a href="../logout.php">Deconnexion</a>  
 
-        <form method="POST" enctype="multipart/form-data">
-          <input type="file" name="fileToUpload" id="fileToUpload">
-          <button id="createFolder" type="submit" name="submit">
-            <img src="../../assets/img/createFolder.svg" alt="folder" />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="../../assets/styles/dashboard.css" />
+</head>
+
+<body>
+  <main id="dashboard">
+    <aside id="user-data">
+      <h1 class="user-welcome">Bonjour <?= $username ?></h1>
+      <p class="user-email"><?= $email ?></p>
+      <a href="../logout.php">Deconnexion</a>
+
+      <form method="POST" enctype="multipart/form-data">
+        <label for="fileToUpload">
+          <button id="createFolder" type="button" name="submit">
+            <img src="../../assets/img/upload.svg" alt="folder" />
           </button>
-        </form>
+        </label>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+      </form>
 
-        <!-- <button id="uploadFile">
-          <img src="../../assets/img/upload.svg" alt="file" />
-        </button> -->
-        
-      </aside>
+    </aside>
 
-      <div id="files-n-folder">
-        <?php if(count($user_files) === 0) { ?>
-            <p class="empty-folder">Votre dossier est vide</p>
+    <div id="files-n-folder">
+      <?php if (count($user_files) === 0) { ?>
+        <p class="empty-folder">Votre dossier est vide</p>
+      <?php } ?>
+
+      <?php foreach ($user_files as $filename) { ?>
+        <?php if (in_array(end(explode('.', $filename)), ['jpg', 'jpeg', 'png', 'gif'])) { ?>
+          <div class="file">
+            <img src="../../assets/img/file.svg" alt="file" />
+            <p><?= $filename ?></p>
+          </div>
+        <?php } else { ?>
+          <div class="folder">
+            <img src="../../assets/img/folder.svg" alt="folder" />
+            <p><?= $filename ?></p>
+          </div>
         <?php } ?>
+      <?php } ?>
+    </div>
+  </main>
+</body>
 
-        <?php foreach($user_files as $filename) { ?>
-          <?php if(in_array(end(explode('.', $filename)), ['jpg', 'jpeg', 'png', 'gif'])) { ?>
-            <div class="file">
-              <img src="../../assets/img/file.svg" alt="file" />
-              <p><?= $filename ?></p>
-            </div>
-          <?php } else { ?>
-            <div class="folder">
-              <img src="../../assets/img/folder.svg" alt="folder" />
-              <p><?= $filename ?></p>
-            </div>
-          <?php } ?>
-        <?php } ?>
-      </div>
-    </main>
-  </body>
 </html>
